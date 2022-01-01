8467a31e530c56e4898ff813fb8575bd23cbde35
tachi7830 <xwsmilexw@gmail.com>
2022-01-01 22:09:58 +0900

    progate:lesson4

--- app.js
+++ app.js
@@ -15,3 +15,12 @@ animal.greet();
 
 // Animalクラスを定義してください
 class Animal{}
+
+class Animal {
+}
+
+// Animalクラスのインスタンスを定数animalに代入してください
+const animal = new Animal();
+
+// 定数animalの値を出力してください
+console.log(animal);
