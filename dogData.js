00ae2b1393f3ab447ad48006be9c2afac6b0d54d
tachi7830 <xwsmilexw@gmail.com>
2022-01-06 13:28:17 +0900

    progate:lesson5

--- dogData.js
+++ dogData.js
@@ -1,5 +1,8 @@
-// Dogクラスのインポートと定数dogを以下に張り付けてください
 import Dog from "./dog";
-const dog = new Dog("レオ", 4, "チワワ");
-// 定数dogをエクスポートしてください
-export default dog;
+
+// 定数dog1, dog2を確認してください
+const dog1 = new Dog("レオ", 4, "チワワ");
+const dog2 = new Dog("ベン", 2, "プードル");
+
+// 以下を書き換えて、定数dog1, dog2をエクスポートしてください
+export {dog1,dog2};
