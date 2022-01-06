f0504b35c915b21408da514ba60cbf6042744a96
tachi7830 <xwsmilexw@gmail.com>
2022-01-06 16:28:22 +0900

    progate:lesson6

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-const characters = ["にんじゃわんこ", "ベイビーわんこ", "ひつじ仙人", "とりずきん"];
+const numbers = [1, 3, 5, 7, 9];
 
-// forEachメソッドを使って、配列charactersの中身をすべて出力してください
+// findメソッドを使って配列numbersから3の倍数を見つけ、定数foundNumberに代入してください
+const foundNumber = numbers.find((number) => { 
+  return number % 3 === 0;
+});
 
-characters.forEach((character) => {console.log(character);});
+// foundNumberを出力してください
+console.log(foundNumber);
+
+
+const characters = [
+  {id: 1, name: "にんじゃわんこ", age: 6},
+  {id: 2, name: "ベイビーわんこ", age: 2},
+  {id: 3, name: "ひつじ仙人", age: 100},
+  {id: 4, name: "とりずきん", age: 21}
+];
+
+// 定数charactersからidが3のオブジェクトを見つけ、定数foundCharacterに代入してください
+const foundCharacter = characters.find((character) => { 
+  return character.id === 3;
+});
+
+// foundCharacterを出力してください
+console.log(foundCharacter);
