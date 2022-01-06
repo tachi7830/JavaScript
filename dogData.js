f0b3d1ff4d628cc3df154a98d2ae9222e3dc8a4c
tachi7830 <xwsmilexw@gmail.com>
2022-01-06 14:36:06 +0900

    progate:lesson5 finish

--- dogData.js
+++ dogData.js
@@ -1,8 +1,19 @@
-import Dog from "./dog";
+// readline-syncをインポートしてください
+import readlineSync from "readline-sync";
 
-// 定数dog1, dog2を確認してください
+import Dog from "../class/dog";
+
 const dog1 = new Dog("レオ", 4, "チワワ");
-const dog2 = new Dog("ベン", 2, "プードル");
 
-// 以下を書き換えて、定数dog1, dog2をエクスポートしてください
-export {dog1,dog2};
+// readlineSync.questionを使って書き換えてください
+const name = readlineSync.question("名前を入力してください: ");
+
+// readlineSync.questionIntを使って書き換えてください
+const age = readlineSync.questionInt("年齢を入力してください: ");
+
+// readlineSync.questionを使って書き換えてください
+const breed = readlineSync.question("犬種を入力してください: ");
+
+const dog2 = new Dog(name, age, breed);
+
+export { dog1, dog2 };
