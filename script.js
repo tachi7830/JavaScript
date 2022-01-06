0ce722afaf79239a63131577010959d3bf6d4a24
tachi7830 <xwsmilexw@gmail.com>
2022-01-06 22:47:51 +0900

    progate:lesson7

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ const printWanko = () => {
   console.log("にんじゃわんこ");
 };
 
-const printHitsuji = () => {
-  console.log("ひつじ仙人");
-};
-
+// 関数callにcallbackという名前の引数を追加してください
 const call = (callback) => {
   console.log("コールバック関数を呼び出します。");
+  // 引数に渡したcallbackを呼び出してください
   callback();
 };
 
-// 引数をprintHitsujiに書き換えて出力を確認しましょう
-call(printHitsuji);
+// 関数printWankoを引数に渡して関数callを実行してください
+call(printWanko);
