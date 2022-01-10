2bdffae14c9c5d5e464fa64be7b9eb23f66b67ff
tachi7830 <xwsmilexw@gmail.com>
2022-01-10 11:33:59 +0900

    progate:jQuery lesson1

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 $(function() {
-  // 「#change-css」要素に対するclickイベントを作成してください
-  $("#change-css").click(function(){
-    $("#text").css("color","red");
-    $("#text").css("font-size","50px");
-  })
-    
+  // 「#change-text」要素に対するclickイベントを作成してください
+  $("#change-text").click(function(){
+    $("#text").text("ようこそ、Progateへ");
+  });
+  
+  // 「#change-html」要素に対するclickイベントを作成してください
+  $("#change-html").click(function(){
+    $("#text").html('<a href="https://prog-8.com/">ようこそ、Progateへ</a>');
+  });
 });
