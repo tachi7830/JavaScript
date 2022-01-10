42265525d0de791045b5d6b9f064b66328127c78
tachi7830 <xwsmilexw@gmail.com>
2022-01-10 12:23:30 +0900

    progate:jQuery lesson1

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 $(function() {
-  $('.btn').click(function() {
-    // $('#title')を変数$titleに代入してください
-    var $title = $("#title");
+  $('#find-method').click(function() {
+    // findメソッドで、「#wrapper」内にあるすべての「a」要素を取得し、
+    // cssメソッドで文字の色をredに指定してください
+    $("#wrapper").find("a").css("color","red");
     
     
-    // 「#title」に対する3つのメソッドを、変数を使って書き換えてください
-    $title.css('color', 'red');
-    $title.html('こんばんは、にんじゃわんこさん');
-    $title.fadeOut(1000);
+  });
+  
+  $('#children-method').click(function() {
+    // childrenメソッドで、「#wrapper」の一階層下にある「a」要素を取得し、
+    // fadeOutメソッドで隠してください
+    $("#wrapper").children("a").fadeOut();
     
-    // 「#text」に対する3つのメソッドを、メソッドチェーンを使って書き換えてください
-    $('#text').css('color', 'blue').html('<h3>jQueryをマスターしましょう！</h3>').fadeOut(1000);
+    
   });
 });
