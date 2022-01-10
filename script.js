84e66741febd5a06a9e1417ab9bf928de92b9ec2
tachi7830 <xwsmilexw@gmail.com>
2022-01-10 11:12:18 +0900

    progate:jQuery lesson1

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(function() {
-  // 「#hide-text」要素に対するclickイベントを作成してください
-  $("#hide-text").click(function(){
-    $("#text").slideUp();
+  // 「#change-css」要素に対するclickイベントを作成してください
+  $("#change-css").click(function(){
+    $("#text").css("color","red");
+    $("#text").css("font-size","50px");
   })
-  
+    
 });
