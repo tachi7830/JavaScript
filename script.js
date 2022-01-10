a94ec1033e265ab6fb90fa06cce79a42325c005c
tachi7830 <xwsmilexw@gmail.com>
2022-01-10 13:36:11 +0900

    progate:jQuery lesson2

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// jQueryの型を用意してください
-$(function(){
+$(function() {
+  // 「#login-show」要素に対するclickイベントを作成してください
+  $("#login-show").click(function(){
+    $("#login-modal").fadeIn();
+  });
   
 });
