7bfc2a34bf1e83dd9bbebae990471193130c77ff
tachi7830 <xwsmilexw@gmail.com>
2022-01-11 16:04:41 +0900

    progate:jQuery lesson3 finish

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-$(function(){
-  $(".signup-show").click(function() {
-    $("#signup-modal").fadeIn();
+$(function() {
+
+  // 新規登録モーダル
+
+  $('.signup-show').click(function() {
+    $('#signup-modal').fadeIn();
   });
+
+  $('#close-modal').click(function() {
+    $('#signup-modal').fadeOut();
+  });
+
+  // 言語一覧
+
+  $('.lesson').hover(
+    function() {
+      $(this).find('.text-contents').addClass('text-active');
+    },
+    function() {
+      $(this).find('.text-contents').removeClass('text-active');
+    }
+  );
   
-  $('#close-modal').click(function() {            
-    $('#signup-modal').fadeOut();            
+  $('.faq-list-item').click(function() {            
+    var $answer = $(this).find('.answer');            
+    if($answer.hasClass('open')) {            
+      $answer.removeClass('open');            
+      $answer.slideUp();
+      $(this).find('span').text('+');            
+    } else {            
+      $answer.addClass('open');            
+      $answer.slideDown();
+      $(this).find('span').text('-');            
+    }            
   });
 });
-
