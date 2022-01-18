d13370dca2f262a788d3e49e4db7c60ac65c1eea
tachi7830 <xwsmilexw@gmail.com>
2022-01-18 21:25:30 +0900

    progate:jQuery lesson4

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(function() {
-  // toggleChangeBtn関数を定義してください
-  function toggleChangeBtn (){
+  
+  function toggleChangeBtn() {
     var slideIndex = $('.slide').index($('.active'));
     $('.change-btn').show();
     if (slideIndex == 0) {
       $('.prev-btn').hide();
-    } else if (slideIndex == 3) {
+    // 「3」の部分を、lengthメソッドを用いて書き換えてください
+    } else if (slideIndex == $('.slide').length - 1) {
       $('.next-btn').hide();
     }
   }
@@ -14,8 +15,6 @@ $(function() {
     $('.active').removeClass('active');
     var clickedIndex = $('.index-btn').index($(this));
     $('.slide').eq(clickedIndex).addClass('active');
-    
-    // 以下をtoggleChangeBtn関数にまとめ、関数を呼び出すようにしてください
     toggleChangeBtn();
   });
   
@@ -27,8 +26,6 @@ $(function() {
     } else {
       $displaySlide.prev().addClass('active');
     }
-    
-    // 以下をtoggleChangeBtn関数にまとめ、関数を呼び出すようにしてください
     toggleChangeBtn();
   });
 });
