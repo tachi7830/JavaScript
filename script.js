e15180463849238c0d52f503d2709afe4a67b6f7
tachi7830 <xwsmilexw@gmail.com>
2022-01-18 22:01:04 +0900

    progate:jQuery lesson4

--- script.js
+++ script.js
@@ -1,31 +1,10 @@
 $(function() {
   
-  function toggleChangeBtn() {
-    var slideIndex = $('.slide').index($('.active'));
-    $('.change-btn').show();
-    if (slideIndex == 0) {
-      $('.prev-btn').hide();
-    // 「3」の部分を、lengthメソッドを用いて書き換えてください
-    } else if (slideIndex == $('.slide').length - 1) {
-      $('.next-btn').hide();
-    }
-  }
-  
-  $('.index-btn').click(function() {
-    $('.active').removeClass('active');
-    var clickedIndex = $('.index-btn').index($(this));
-    $('.slide').eq(clickedIndex).addClass('active');
-    toggleChangeBtn();
+  // 「#form」要素のsubmitイベントを作成してください
+  $('#form').submit(function(){
+    var textValue = $('#text-form').val();
+    $('#output-text').text(textValue);
+    return false;
   });
-  
-  $('.change-btn').click(function() {
-    var $displaySlide = $('.active');
-    $displaySlide.removeClass('active');
-    if ($(this).hasClass('next-btn')) {
-      $displaySlide.next().addClass('active');
-    } else {
-      $displaySlide.prev().addClass('active');
-    }
-    toggleChangeBtn();
-  });
+    
 });
